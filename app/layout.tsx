a8bbe1b5ce293f55ccb1d2a541ca62f38c0077f9
sakib3046 <sakib3046@gmail.com>
2026-02-21 23:00:12 +0600

    Facebook-domain-verification content="tmpin48vno7ppm67u2fdjgq9h5adjd" />

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,6 +41,7 @@ export default function RootLayout({
       <head>
         <meta name="google-adsense-account" content="ca-pub-5440986495958060" />
         <meta name="facebook-domain-verification" content="tmpin48vno7ppm67u2fdjgq9h5adjd" />
+        <meta name="facebook-domain-verification" content="tmpin48vno7ppm67u2fdjgq9h5adjd" />
       </head>
       <body className={`${inter.variable} antialiased`} suppressHydrationWarning>
         {/* Google Tag Manager (noscript) */}
